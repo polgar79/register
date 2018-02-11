d9b960f1fcd538801c0cee6034b89c181e81a8db
polgar79 <polgar79@gmail.com>
2018-02-11 19:33:34 +0100

    Deleted text

--- index1.php
+++ index1.php
@@ -1,15 +1,14 @@
 <?php 
 session_start();
-echo $_SESSION['OID'];
+//echo $_SESSION['OID'];
 
 if (isset($_SESSION['OID']))
   {
 //include_once  'class/db.php';
 include 'includes/header.php';
 ?>
-<h1>London</h1>
-<p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-<p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+<h1>Adminisztráció</h1>
+
 <?php 
 
 include 'includes/footer.php';
