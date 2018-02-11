b15a4b571fc71e02430bed81dbb445af6c38118d
polgar79 <polgar79@gmail.com>
2018-02-11 19:32:43 +0100

    Add dinamic page title

--- includes/header.php
+++ includes/header.php
@@ -1,46 +1,57 @@
-<?php include 'class/db.php'; ?>
+<?php 
+include 'class/db.php'; 
+include 'lang/lang.php';
+include 'includes/functions.php';
+
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <link rel="stylesheet" href="css/style.css">
-<title>Header</title> 
+<title><?php echo $title = dinamicPageTitle(); ?></title> 
 </head>
 
 <body>
 <div class="container">
 
 <div id ="header">
-   <h3>Nyilvántartás</h3>
+   <h3><?php echo PROJECTNAME ;?></h3>
 </div>
 <div class ="nav2">
   		<ul id="menu">
-  			<li><a href="index1.php">Főoldal</a></li>
+  			<li><a href="index1.php"><?php echo HOME;?></a></li>
   			<li>
-  			<a href="">Személyek kezelése</a>
+  			<a href=""><?php echo U_MANAGER;?></a>
   				<div>
-  				<a href="add_user.php">Befizető felvétele</a>
-  				<a href="userlist.php">Adatmódosítás</a>
+  				<a href="add_user.php"><?php echo ADD_PA_USER;?></a>
+  				<a href="userlist.php"><?php echo EDIT_U_DATA;?></a>
 
   			
   				</div>
   			</li>
   			<li>
-  			<a href="">Befizetések kezelése</a>
+  			<a href=""><?php echo MANAGE_Y_PAYM;?></a>
   			<div>
-  				<a href="payment.php">Befizetés rögzítése</a>
-  				<a href="search.php">Kimutatások</a>
+  				<a href="payment.php"><?php echo ADD_P;?></a>
+  				<a href="search.php"><?php echo STATEMENTS;?></a>
 
   			
   				</div>
   			
   			
   			</li>
+  		
+  			<li><a href="search.php"><?php echo SEARCH;?></a></li>
   			
+  			<li><a href="#"><?php echo SETTINGS;?></a></li>
   			
-  			
-  			<li><a href="search.php">Keresés</a></li>
-  			<li><a href="logout.php">Kijelentkezés</a></li>
+  			<li><a href="logout.php"><?php echo LOGOUT;?></a></li>
   		
   		</ul>
 	  </div>
