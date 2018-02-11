20dcd2dad81920abf400b5799a9a3bda7766235b
polgar79 <polgar79@gmail.com>
2018-02-11 19:31:10 +0100

    Add lang.php

---
+++ lang/lang.php
@@ -0,0 +1,18 @@
+<?php 
+/* header.php*/
+define("PROJECTNAME", "Nyilvántartás");
+define("HOME", "Főoldal");
+define("SEARCH", "Keresés");
+define("LOGOUT", "Kijelentkezés");
+define("SETTINGS", "Beállítások");
+define("MANAGE_Y_PAYM", "Befizetések kezelése");
+define("ADD_P", "Befizetés rögzítése");
+define("STATEMENTS", "Kimutatások");
+define("EDIT_U_DATA", "Adatmódosítás");
+define("ADD_PA_USER", "Befizető felvétele");
+define("U_MANAGER", "Személyek kezelése");
+
+/*functions.php*/
+define("HOMETITLE", "Főoldal");
+
+?>
